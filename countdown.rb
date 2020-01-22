e38157707d67925fe2bbe094f6b77ed03d1f68fd
David Van Fleet <dvfleet413@gmail.com>
2020-01-22 20:59:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+  while seconds > 0 do
+    puts "#{seconds} SECONDS!" if seconds > 1
+    puts "#{seconds} SECOND!" if seconds == 1
+    seconds -= 1
+  end 
+  return "HAPPY NEW YEAR!"
 end
